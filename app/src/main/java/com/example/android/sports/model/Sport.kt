9ed518b9065d84d74d9ef400be721c585198a07c
Denis Moroz <moroz06denis@gmail.com>
2023-10-08 20:37:26 +0200

    added implementation of sports app

---
+++ app/src/main/java/com/example/android/sports/model/Sport.kt
@@ -0,0 +1,13 @@
+package com.example.android.sports.model
+
+import com.example.android.sports.R
+
+data class Sport(
+    val id: Int,
+    val titleResourceId: Int,
+    val subTitleResourceId: Int,
+    val imageResourceId: Int,
+    val sportsImageBanner: Int,
+    val newsDetails: Int = R.string.sports_news_detail_text
+)
+
